e937a638f6437624e3fa896a23a246c4fdaf3e61
Yevhen Krupin (ykn) <ykn@tobii.com>
2018-02-21 15:00:40 +0200

    handle application closing event

--- Cef/ViewModels/MainViewModel.cs
+++ Cef/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Threading;
 using Cef.Views;
+using CefSharp;
 using CommonsLib;
 
 namespace Cef
@@ -11,12 +12,15 @@ namespace Cef
     public class MainViewModel : ViewModel
     {
         private readonly Dispatcher _dispatcher;
+        private ToolsWindow _toolsWindow;
         public BrowserTabSelectorViewModel BrowserTabSelectorViewModel { get; private set; }
 
         public ToolsViewModel ObservationViewModel { get; set; }
 
         public ICommand OpenToolsCommand { get; private set; }
-        
+
+        public ICommand CloseApplicationCommand { get; private set; }
+
         public MainViewModel(string defaultUrl, Dispatcher dispatcher)
         {
             _dispatcher = dispatcher;
@@ -24,15 +28,22 @@ namespace Cef
             BrowserTabSelectorViewModel = new BrowserTabSelectorViewModel(defaultUrl, logger, dispatcher);
             ObservationViewModel = new ToolsViewModel(logger, BrowserTabSelectorViewModel);
             OpenToolsCommand = new DelegateCommand(OpenToolsExecute);
+            CloseApplicationCommand = new DelegateCommand(CloseApplicationExecute);
+        }
+
+        private void CloseApplicationExecute()
+        {
+            BrowserTabSelectorViewModel.SelectedTab.Browser.CloseDevTools();
+            _dispatcher.Invoke(_toolsWindow.Close);
         }
         
         private void OpenToolsExecute()
         {
             _dispatcher.InvokeAsync(() =>
             {
-                var window = new ToolsWindow() { Topmost = true};
-                window.DataContext = ObservationViewModel;
-                window.Show();
+                _toolsWindow = new ToolsWindow() { Topmost = true};
+                _toolsWindow.DataContext = ObservationViewModel;
+                _toolsWindow.Show();
             });
         }
     }
